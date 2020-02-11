2e236e391159a50a43a1931e81a96ea960cc6021
Henry Blevins <heblevi@gmail.com>
2020-02-10 23:48:20 -0500

    Recitation complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import './App.scss';
 
+const cn = (...args) => {
+  return args.filter(x => x).join(' ');
+};
+
 function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    if (isRunning) {
+      const id = window.setInterval(() => {
+        setSeconds(seconds => seconds+1);
+      }, 1000);
+      return () => window.clearInterval(id);
+    }
+    return undefined;
+  }, [isRunning]);
+
   return (
     <div className="app">
-      <div className='time-circle'>
+      <div className={cn('time-circle', !isRunning && 'paused')}>
         <div className="time">
-          00:04
+          {seconds}
         </div>
       </div>
       <div className="buttons">
-        <button className="play-pause">
-          <i className="fa fa-play fa-2x" />
-        </button>
-        <button className="play-pause">
-          <i className="fa fa-pause fa-2x" />
-        </button>
-        <button className="reset">
+        {isRunning
+         ? (
+           <button className="play-pause" onClick={() => {
+             setIsRunning(false);
+           }}>
+             <i className="fa fa-pause fa-2x" />
+           </button>
+         )
+         : (
+           <button
+             className="play-pause"
+             onClick={() => setIsRunning(true)}>
+             <i className="fa fa-play fa-2x" />
+           </button>
+         )
+        }
+        <button
+          disabled={!isRunning}
+          className="reset"
+          onClick={() => {
+            setIsRunning(false);
+            setSeconds(0);
+          }}>
           Reset
         </button>
       </div>
